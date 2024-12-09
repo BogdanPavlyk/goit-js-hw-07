d2a6452f20c755180fc3f02b572bdde1c02655c0
Bogdan Pavlyk <bogdan.pavlyk7@gmail.com>
2024-12-09 06:05:26 +0200

    2-6 tasks pages

--- js/task-2.js
+++ js/task-2.js
@@ -1,8 +1,28 @@
-function getShippingMessage(country, price, deliveryFee) {
-  const totalPrice = price + deliveryFee;
-  return `Shipping to ${country} will cost ${totalPrice} credits`;
+const images = [
+  {
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
+  },
+];
+
+const gallery = document.querySelector(".gallery");
+
+function getGalleryItemsMarkup(items) {
+  return items
+    .map(({ url, alt }) => {
+      return `<li>
+          <img src=${url} alt=${alt} />
+          </li>`;
+    })
+    .join("");
 }
 
-console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
-console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+gallery.insertAdjacentHTML("afterbegin", getGalleryItemsMarkup(images));
--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,13 @@
-function getElementWidth(content, padding, border) {
-  return parseFloat(content) + parseFloat(padding) * 2 + parseFloat(border) * 2;
-}
+const nameInput = document.querySelector("#name-input");
+const nameOutput = document.querySelector("#name-output");
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+nameInput.addEventListener("input", (event) => {
+  const element = event.target;
+  const value = element.value.trim();
+  
+  if (!value) {
+    nameOutput.textContent = "Anonymous";
+  } else {
+    nameOutput.textContent = value;
+  }
+});
